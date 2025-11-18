220b166b5266f78c9a477e2b928789139842ef1b
KEVIN DE BRITO HOLANDA <45214308809@imaccommand026.local>
2025-11-18 11:28:53 -0300

    exercicios02

---
+++ exercicios02.js
@@ -0,0 +1,42 @@
+let Number = window.prompt("Digite um número de 1 a 7")
+
+switch (Number) {
+    case "1":
+        console.log("Segunda-Feira");
+        
+        break;
+
+    case "2":
+        console.log("Terça-Feira")
+
+        break;
+
+        case "3":
+            console.log("Quarta-Feira")
+
+            break;
+
+        case "4":
+            console.log("Quinta-feira")
+
+            break;
+
+        case "5": 
+        console.log("Sexta-Feira")
+
+        break;
+
+        case "6":
+            console.log("Sabádo")
+
+            break;
+
+        case "7":
+            console.log("Domingo")
+
+            break;
+
+    default:
+        console.log("Não é um número de 1 a 7")
+        break;
+}
