648b10e944b5ee33046054d5487501f38668e10a
KEVIN DE BRITO HOLANDA <45214308809@imaccommand026.local>
2025-11-18 14:42:55 -0300

    exercicios05

---
+++ exercicios05.js
@@ -0,0 +1,14 @@
+let numero01 = Number.parseFloat(prompt("Digite o primeiro número"))
+let numero02 = Number.parseFloat(prompt("Digite o segundo número"))
+let numero03 = Number.parseFloat(prompt("Digite o terceiro número")) 
+
+if (numero01 < numero02 && numero02 , numero03) 
+
+   { console.log ("A sequência do número está em ordem crescente"), numero01, numero02, numero03
+
+}
+
+else {
+    console.log("A Sequência do número não está em ordem crescente") }
+
+
