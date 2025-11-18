794587d2c4078f816b45e4bb230c4afea4ee8136
KEVIN DE BRITO HOLANDA <45214308809@imaccommand026.local>
2025-11-18 12:05:25 -0300

    exercicios03

---
+++ exercicios03.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let Numero = parseInt(window.prompt("Informe o número inteiro") )
+if (Numero % 2 == 0) {
+    console.log("O Número é par")
+}
+
+else{
+    console.log(" O Número é Impar")
+}
